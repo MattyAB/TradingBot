1e2c0bca133f32d5a0dea446db20b6ba67ed9762
MattyAB <matthew.beton@gmail.com>
2019-01-08 18:04:52 +0000

    Initial commit

---
+++ TradingBot/DataLoader.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TradingBot
+{
+    internal class DataLoader
+    {
+        public DataLoader(string path)
+        {
+            // Create stopwatch to give estimate of data load time
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            // Load data from file
+            string data = LoadData(path);
+            // Deserialize data
+            List<DataPoint> dataPoints = DeserializeJson(data);
+
+            // Tell the user how long it took
+            watch.Stop();
+            Console.WriteLine("Loaded and deserialized " + 
+                (dataPoints[dataPoints.Count - 1].openTime - dataPoints[0].openTime).TotalDays + " days of data in " + 
+                watch.Elapsed.TotalSeconds.ToString() + " seconds.");
+        }
+
+        List<DataPoint> DeserializeJson(string data)
+        {
+            List<string[]> DataRaw = JsonConvert.DeserializeObject<List<string[]>>(data);
+
+            List<DataPoint> output = new List<DataPoint>();
+
+            for(int i = 0; i < DataRaw.Count; i++)
+            //foreach(string[] Data in DataRaw)
+            {
+                string[] Data = DataRaw[i];
+                DataPoint Point = new DataPoint();
+
+                DateTime openTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                Point.openTime = openTime.AddMilliseconds(Convert.ToInt64(Data[0]));
+                Point.open = Convert.ToDouble(Data[1]);
+                Point.high = Convert.ToDouble(Data[2]);
+                Point.low = Convert.ToDouble(Data[3]);
+                Point.close = Convert.ToDouble(Data[4]);
+                Point.volume = Convert.ToDouble(Data[5]);
+                DateTime closeTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+                Point.closeTime = closeTime.AddMilliseconds(Convert.ToInt64(Data[6]));
+                Point.quoteVolume = Convert.ToDouble(Data[7]);
+                Point.tradeCount = Convert.ToInt64(Data[8]);
+                Point.buyBase = Convert.ToDouble(Data[9]);
+                Point.buyQuote = Convert.ToDouble(Data[10]);
+
+                output.Add(Point);
+            }
+
+            return output;
+        }
+
+        string LoadData(string path)
+        {
+            return System.IO.File.ReadAllText(path);
+        }
+    }
+
+    class DataPoint
+    {
+        public double open;
+        public double high;
+        public double low;
+        public double close;
+        public double volume;
+        public double quoteVolume;
+        public long tradeCount;
+        public double buyBase;
+        public double buyQuote;
+        public DateTime openTime;
+        public DateTime closeTime;
+    }
+}
---
+++ TradingBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TradingBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataLoader dl = new DataLoader(@"E:\Documents\Code\C#\TradingBot\data.json");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
