8f61955aacf143b8518e4b5a5e64001ccff1e916
MattyAB <matthew.beton@gmail.com>
2019-01-09 20:49:14 +0000

    Created Indicators class ready for indicator generation code.

---
+++ TradingAlgorithm/Indicators.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingAlgorithm
+{
+    class Indicators
+    {
+        public DataPoint Tick(DataPoint Point)
+        {
+            return Point;
+        }
+    }
+}
--- TradingAlgorithm/TradingAlgorithm.cs
+++ TradingAlgorithm/TradingAlgorithm.cs
@@ -4,14 +4,16 @@ namespace TradingAlgorithm
 {
     public class TradingAlgorithm
     {
+        private Indicators indicators;
+
         public TradingAlgorithm()
         {
-
+            indicators = new Indicators();
         }
 
         public void Tick(DataPoint Point)
         {
-
+            Point = indicators.Tick(Point);
         }
     }
 }
