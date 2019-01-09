0e71ca0ab07e03c3ddffab522135c5ff0a43e634
MattyAB <matthew.beton@gmail.com>
2019-01-09 17:58:34 +0000

    Added initial wallet class. Will hold current state of portfolio.

---
+++ TradingBot/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingBot
+{
+    class Wallet
+    {
+        private double BTCBalance;
+        private double USDTBalance;
+
+        public Wallet(double RawBTC, double RawUSDT) // Needs to also take a list of current positions
+        {
+            BTCBalance = RawBTC;
+            USDTBalance = RawUSDT;
+        }
+    }
+}
