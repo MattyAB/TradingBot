ccfd877219c9db6738464c13085a877965c735a4
MattyAB <matthew.beton@gmail.com>
2019-01-24 16:17:49 +0000

    Started writing position. some pretty vital code here.

--- TradingAlgorithm/Position.cs
+++ TradingAlgorithm/Position.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace TradingAlgorithm
 {
     class Position
     {
-        public Position(bool longOrShort, int opener)
+        private bool longOrShort;
+        private int openerUsed;
+        private DataPoint OpeningPoint;
+
+
+        public double takeProfit;
+        public double stopLoss;
+
+        public Position(bool longOrShort, int opener, DataPoint OpeningPoint)
         {
-            throw new NotImplementedException();
+            this.longOrShort = longOrShort;
+            openerUsed = opener;
+            this.OpeningPoint = OpeningPoint;
+
+            if (longOrShort)
+            {
+                // Long
+                // Aim to make 5%
+                takeProfit = OpeningPoint.close * 1.05;
+                // Cut losses at 2%
+                stopLoss = OpeningPoint.close * 0.98;
+            }
+            else
+            {
+                // Short
+                // Aim to make 5%
+                takeProfit = OpeningPoint.close * 0.95;
+                // Cut losses at 2%
+                stopLoss = OpeningPoint.close * 1.02;
+            }
         }
     }
 }
