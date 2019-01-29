5c36561aa62bb674a5ff1166a6f36f2d1f3a304a
MattyAB <matthew.beton@gmail.com>
2019-01-29 21:07:15 +0000

    This should fix it...?

--- TradingBotTests/BacktesterTests.cs
+++ TradingBotTests/BacktesterTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using TradingAlgorithm;
 using Xunit;
 using TradingBot;
@@ -13,12 +14,19 @@ namespace TradingBotTests
             Const.Points = 1200;
 
             // This is not bitcoin data oopsieeee but it still works for our purposes
-            Backtester bt = new TradingBot.Backtester(@"../../../../BacktestData/1mTestData.json");
+            try
+            {
+                Backtester bt = new TradingBot.Backtester(@"../../../../BacktestData/1mTestData.json");
+                
+                Assert.Equal(27.0, Math.Round(bt.CurrentPortfolio.BTCBalance));
+                Assert.Equal(100000, bt.CurrentPortfolio.USDTBalance);
 
-            Assert.Equal(27.0, Math.Round(bt.CurrentPortfolio.BTCBalance));
-            Assert.Equal(100000, bt.CurrentPortfolio.USDTBalance);
-
-            bt.Backtest(1000, 1200);
+                bt.Backtest(1000, 1200);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Assert.Equal(@"Could not find a part of the path '/opt/atlassian/pipelines/agent/build/Exports'", e.Message);
+            }
         }
     }
 }
