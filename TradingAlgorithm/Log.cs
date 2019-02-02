e78f65ce9c12d0b04922dfc11eaec6242f1275d3
MattyAB <matthew.beton@gmail.com>
2019-02-02 09:30:40 +0000

    Neater final logging (less wordey - phew!)

--- TradingAlgorithm/Log.cs
+++ TradingAlgorithm/Log.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Threading;
 
 namespace TradingAlgorithm
 {
@@ -39,10 +40,23 @@ namespace TradingAlgorithm
         /// <param name="endValueFluct">Finishing portfolio value, calculated with finishing price (subject to price fluctuations)</param>
         public static void FinishUp(double startValue, double endValueConst, double endValueFluct, int longPos, int shortPos)
         {
+            /**
             Console.WriteLine("All done! Started with a portfolio value of " + startValue + "...");
             Console.WriteLine("Mesaured with the start price, we finish with a value of " + endValueConst + "! This is a percentage of " + Math.Round(((endValueConst - startValue) / startValue) * 100) + "%");
             Console.WriteLine("But if we measure with the end price, we finish with a value of " + endValueFluct + "! This is a percentage of " + Math.Round(((endValueFluct - startValue) / startValue) * 100) + "%");
             Console.WriteLine("Total of " + longPos + " long positions and " + shortPos + " short pos");
+            **/
+
+            Console.WriteLine();
+            Console.WriteLine(new String('-', 5) + " VALUE " + new String('-', 5));
+            Console.WriteLine("Start value : " + startValue);
+            Console.WriteLine(new String('-', 20));
+            Console.WriteLine("End value : " + endValueConst + "(" + Math.Round(((endValueConst - startValue) / startValue) * 100) + "%)  from start BTC value");
+            Console.WriteLine(new String('-', 20));
+            Console.WriteLine("End value : " + endValueFluct + "(" + Math.Round(((endValueFluct - startValue) / startValue) * 100) + "%)  from final BTC value");
+            Console.WriteLine();
+            Console.WriteLine(new String('-', 5) + " POSITIONS " + new String('-', 5));
+            Console.WriteLine("Long: " + longPos + "    Short: " + shortPos);
         }
     }
 }
