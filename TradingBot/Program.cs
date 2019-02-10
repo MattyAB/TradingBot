b4a9238f3179619ca0eabcc5277695082502d9a6
MattyAB <matthew.beton@gmail.com>
2019-02-10 21:15:53 +0000

    Far streamlined the process - now has ij start and end variables, as well how many steps between each variable. This makes it a lot neater for frequent use.

--- TradingBot/Program.cs
+++ TradingBot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 using TradingAlgorithm;
 
@@ -27,30 +30,62 @@ namespace TradingBot
         {
             Const.log = false;
 
-            double[][] result = new double[20][];
+            // I variables
+            double iStart = 0.085;
+            double iEnd = 0.1;
+            int Iend = 5;
+            double iStep = (iEnd - iStart) / (Iend + 1);
 
-            // Gather data - this takes a while.
-            Parallel.For(0, 20, i =>
+            // J variables
+            double jStart = 0.015;
+            double jEnd = 0.03;
+            int Jend = 5;
+            double jStep = (iEnd - iStart) / (Iend + 1);
+
+            double[][] result = new double[Iend + 1][];
+
+            // Load Data
+            DataLoader dl = new DataLoader(Const.backtestPath);
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            // Gather data - this takes a while
+            for (int i = 0; i <= Iend; i++)
             {
-                Console.WriteLine(i);
+                double I = iStart + (i * iStep);
+                Const.TPPercentage = I;
 
-                // Load Data
-                DataLoader dl = new DataLoader(Const.backtestPath);
+                Console.WriteLine("i: " + I);
+                result[i] = new double[Jend + 1];
 
-                Console.WriteLine("L" + i);
-                result[i] = new double[20];
-                for (int j = 0; j < 20; j++)
+                for (int j = 0; j <= Jend; j++)
                 {
-                    Const.TPPercentage = (double)(i + 1) / 200;
-                    Const.SLPercentage = (double)(j + 1) / 200;
+                    try
+                    {
+                        int soFar = ((i * (Jend + 1)) + j);
+                        long seconds = s.ElapsedMilliseconds / 1000;
+                        double secondsPer = seconds / soFar;
+
+                        double secondsRemaining = secondsPer * (((Iend + 1) * (Jend + 1)) - soFar);
+                        Console.Write(secondsRemaining / 60 + " remaining: ");
+                    }
+                    catch(DivideByZeroException e)
+                    {
+                        Console.WriteLine("Dividing by zero.");
+                    }
+
+                    double J = jStart + (j * jStep);
+                    
+                    Console.WriteLine(J);
+                    Const.SLPercentage = J;
+
                     Backtester backtest = new Backtester(dl);
                     result[i][j] = backtest.Backtest(1000, 1500);
 
                     dl.ResetDL();
                 }
-            });
-
-            Console.WriteLine("Done!");
+            }
 
             // Finish by spitting the data into a csv.
             using (StreamWriter outfile = new StreamWriter(@"E:\Documents\Code\C#\TradingBot\Testing\TestTPSL.csv"))
