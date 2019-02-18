a5b3a3a2d3a4e55bd147630586bbb614a67dca11
Matt Beton <matthew.beton@gmail.com>
2019-02-18 22:23:15 +0000

    Imported library
    Imported library and reads file that contains the api key.

--- TradingAlgorithm/Const.cs
+++ TradingAlgorithm/Const.cs
@@ -6,6 +6,7 @@ namespace TradingAlgorithm
 {
     public class Const
     {
+        public const string apiKey = @"E:\Documents\Code\C#\TradingBot\TradingBot\API.txt";
         public const string backtestPath = @"E:\Documents\Code\C#\TradingBot\BacktestData\dataBTCUSDT.json"; // Path for backtest data
         public const string exportPath = @"../../../../Exports/"; // Export path
         public const string trainingValuesPath = @"E:\Documents\Code\C#\TradingBot\Testing\values.csv";
--- TradingBot/Realnet.cs
+++ TradingBot/Realnet.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Timers;
+using Binance.API.Csharp.Client;
+using TradingAlgorithm;
 
 namespace TradingBot
 {
@@ -9,7 +12,11 @@ namespace TradingBot
     {
         public Realnet()
         {
+            string[] keys = File.ReadAllLines(Const.apiKey);
+
             Console.WriteLine("Init realnet");
+            ApiClient apiClient = new ApiClient(keys[0], keys[1]);
+            BinanceClient binanceClient = new BinanceClient(apiClient);
         }
 
         public void Tick(object source, ElapsedEventArgs e)
